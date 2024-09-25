19d150999c5e3e87186b77623bf23a1b7767ba90
siddeshshewde <siddesh.darkeyed95@gmail.com>
2024-09-25 09:53:41 -0400

    Alignment Fix

--- components/EditorComponent.jsx
+++ components/EditorComponent.jsx
@@ -7,9 +7,11 @@ export default function EditorComponent () {
         <div className="min-h-screen dark:bg-slate-900 rounded-2xl shadow-2xl py-6 px-8">
             <div className="flex items-center justify-between">
                 <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0">Zenith IDE</h2>
-                <div className="flex">
+                <div className="flex items-center space-x-2">
                     <ModeToggle />
-                    <SelectLanguage />
+                    <div className="w-[230px]">
+                        <SelectLanguage />
+                    </div>
                 </div>
             </div>
         </div>
--- components/SelectLanguage.jsx
+++ components/SelectLanguage.jsx
@@ -72,9 +72,8 @@ export default function SelectLanguage() {
 
   return (
     <Listbox value={selected} onChange={setSelected}>
-      <Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Label>
-      <div className="relative mt-2">
-        <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+      <div className="relative">
+        <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-2 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
           <span className="flex items-center">
             <img alt="" src={selected.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" />
             <span className="ml-3 block truncate">{selected.name}</span>
