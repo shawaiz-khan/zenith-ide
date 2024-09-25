5054fd47f9aa12458bc27215f8f88af1b2264986
siddeshshewde <siddesh.darkeyed95@gmail.com>
2024-09-25 09:41:50 -0400

    SelectLanguage Component

--- components/EditorComponent.jsx
+++ components/EditorComponent.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { ModeToggle } from "./theme-toggle";
+import SelectLanguage from "./SelectLanguage";
 
 export default function EditorComponent () {
     return (
         <div className="min-h-screen dark:bg-slate-900 rounded-2xl shadow-2xl py-6 px-8">
-            <h2>Editor Component</h2>
-            <ModeToggle />
+            <div className="flex items-center justify-between">
+                <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight first:mt-0">Zenith IDE</h2>
+                <div className="flex">
+                    <ModeToggle />
+                    <SelectLanguage />
+                </div>
+            </div>
         </div>
     )
 }
